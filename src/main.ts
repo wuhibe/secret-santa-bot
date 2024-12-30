8766e4bf7a8d20029d0f2a377cd117d6d56fec30
wuhibe <wuhibetamire710@gmail.com>
2024-12-30 14:55:02 +0300

    feat: add othermodule

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 
 import { AppModule } from './app.module';
+import { OtherModule } from './other.module';
 
 async function bootstrap() {
+  const app = await NestFactory.create(OtherModule);
+  await app.listen(process.env.PORT || 3000);
+  console.log(`API is running on: ${await app.getUrl()}\n`);
+
   await NestFactory.createApplicationContext(AppModule);
 }
 
